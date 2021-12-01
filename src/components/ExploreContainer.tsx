891dc47b4239c6a4ae3793d26bfe74a7c1f6f8a0
Christian Saar <c.saar@sigmatech.de>
2021-12-01 14:09:58 +0100

    Example

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,31 @@
+import { IonButton, useIonLoading, useIonToast } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [showIonLoading, dismissIonLoading] = useIonLoading();
+  const [showIonToast] = useIonToast();
+
+  const handleClick = async() => {
+    try {
+      showIonLoading('Loading');
+      // await new Promise((resolve) => setTimeout(() => resolve(true), 1000));
+      // if (true) {
+      //   throw new Error('An error occurred');
+      // };
+      await fetch('https://uri.invalid', { method: 'GET' });
+      dismissIonLoading();
+      showIonToast('Success', 3000);
+
+    } catch(e) {
+      dismissIonLoading();
+      showIonToast(String(e), 3000);
+    };
+  };
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonButton onClick={handleClick}>Start Loading</IonButton>
   );
 };
 
